9e4733036d3d9c84b50c2e8fc2028d6236acf32b
Shuai Huang <shhuang9532@gmail.com>
2020-04-11 15:16:20 -0700

    Spring-Database

--- src/main/java/com/shuaih/springTest/App.java
+++ src/main/java/com/shuaih/springTest/App.java
@@ -1,5 +1,8 @@
 package com.shuaih.springTest;
 
+import java.util.*;
+
+import jdk.nashorn.internal.runtime.ECMAException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
@@ -27,7 +30,28 @@ public class App
 //        store.requestSupplier();
 
         Connector dbc = (Connector) context.getBean("connector");
+        try {
+//            dbc.createTale();
+            List<Profile> profiles = new ArrayList<Profile>();
 
+            Profile p1 = new Profile("Sta", "Baro", 20);
+            Profile p2 = new Profile("St", "Bar", 40);
+            Profile p3 = new Profile("S", "Ba", 43);
+            Profile p4 = new Profile("A", "B", 27);
+            profiles.add(p1);
+            profiles.add(p2);
+            profiles.add(p3);
+            profiles.add(p4);
+
+            int[] res =  dbc.addProfiles(profiles);
+            for (int i : res)  System.out.println("Updated " + i + " rows");
+
+            List<Profile> ls = dbc.getProfiles();
+            for (Profile p : ls) System.out.println(p);
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
         ((ClassPathXmlApplicationContext) context).close(); // ApplicationContext does not have close()
     }
 }
