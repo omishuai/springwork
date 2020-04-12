fea77259858ca80fc5e2161e35493a0eb9d359a1
Shuai Huang <shhuang9532@gmail.com>
2020-04-12 15:43:46 -0700

    current changes

---
+++ springMVC/src/main/java/com/shuaih/spring/mvc/controllers/controller.java
@@ -0,0 +1,13 @@
+package com.shuaih.spring.mvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class controller {
+
+    @RequestMapping("/")
+    public String showHome() {
+        return "home";
+    }
+}
